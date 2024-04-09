c3b4a90298e358035d6bf62e84df4361e9b6033a
Will Ren <z5429870@ad.unsw.edu.au>
2024-04-10 00:43:05 +1000

    selected columns that should be dropped

--- to_csv.py
+++ to_csv.py
@@ -31,9 +31,15 @@ def visulization_raw_data(df, features_per_plot):
                 plt.title(column)
             else:
                 # for numerical features, use a histogram
-                df[column].replace('', np.nan, inplace=True)
-                sns.histplot(df[column].dropna(), bins=30, kde=False)
+                df[column].replace('', np.nan, inplace=True)  # Replace blanks with NaN
+                cleaned_data = df[column].dropna()  # Drop NaN values for histogram
+                sns.histplot(cleaned_data, bins=30, kde=False)
                 plt.title(column)
+
+                # Count the number of blank or NaN values and display it on the plot
+                blank_or_nan_count = df[column].isna().sum()
+                plt.annotate(f'Blanks/Nan: {blank_or_nan_count}', xy=(0.7, 0.9), xycoords='axes fraction',
+                            fontsize=12, bbox=dict(boxstyle="round", fc="yellow"))
 
         plt.tight_layout()
         plt.show()
@@ -42,9 +48,33 @@ visulization_raw_data(X_train,features_per_plot=10)
 # visulization_raw_data(y_train,features_per_plot=11)
 
 # 1.3 preprocess raw data given the visualization
-# 1.3.1 drop columns with too many missing values
 
+'''
+# 1.3.1 drop columns:
+x_6 as it has 963 'nan' and 37 `0`,
+x_12 as it has 16 'nan' and 984 `0`,
+x_16 as it has 984 `0` and 16 'nan',
+x_17 as it has 983 `0` and 16 'nan' and 1 `1`,
+x_18 as it has 988 `0` and 12 'nan',
+x_19 as it has 987 `0` and 12 'nan' and 1 `1`,
+x_21 as it has 988 `0` and 12 'nan',
+x_22 as it has 986 `0` and 12 'nan' and 2 `1`,
+x_23 as it has 988 `0` and 12 'nan',
+x_24 as it has 987 `0` and 12 'nan' and 1 `1`,
+x_31 as it has 997 `0` and 3 'nan',
+x_40 as it has 991 `0` and 7 'nan' and 2 `1`,
+x_47 as it has 999 `0` and only 1 `1`,
+x_51 as it has 904 `0` and 96 `nan`,
+x_61 as it has 931 `0` and 69 `nan`,
+x_66 as it has 932 `0` and 68 `nan`,
+x_77 as it has 994 `0` and 6 `nan`,
+x_80 as it has 994 `0` and 6 `nan`,
+x_87 as it has 998 `nan`
+'''
 
+# 1.3.2 fill missing values with mode for binary features and mean(rounded to nearest int) for numerical features
+
+# 1.3.3 how about 1 and 0 are neary half, we can fill missing values with 0??
 
 x.fillna(x.mean(), inplace=True)
 x.to_csv('x.csv', index=False)
